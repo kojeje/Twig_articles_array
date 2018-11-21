9ab63eea6d631ad9e8d092d0674737c75a0d6b54
kojeje <kojeje.dev@gmail.com>
2018-11-21 10:42:15 +0100

    Boucles articles nom de fichiers img en concat

--- src/AppBundle/Controller/PagesController.php
+++ src/AppBundle/Controller/PagesController.php
@@ -5,44 +5,128 @@
      * Date: 20/11/2018
      * Time: 14:16
      */
-// identifiant de classes (namespace
+
+
     namespace AppBundle\Controller;
     //autoloader
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-    use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
-    use Symfony\Component\HttpFoundation\Request;
+
 
     class PagesController extends Controller
     {
+        private $articles =
+                [
+                    'posts' =>
+                        [
+
+                        'id' => '01',
+                        'titre' => 'Lorem 1',
+                        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                                 Obcaecati provident quam suscipit tempore velit. <br>
+                                                 Assumenda consequuntur expedita magnam officia recusandae
+                                                  reiciendis repudiandae,
+                                                  sed sit!<br>
+                                                 Accusantium dignissimos nobis rerum sequi. Molestias.'
+                        ],
+                        [
+
+                        'id' => '02',
+                        'titre' => 'Lorem 2',
+                        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                                     Obcaecati provident quam suscipit tempore velit. <br>
+                                                     Assumenda consequuntur expedita magnam officia recusandae
+                                                      reiciendis repudiandae,
+                                                      sed sit!<br>
+                                                     Accusantium dignissimos nobis rerum sequi. Molestias.'
+                        ],
+                        [
+
+                        'id' => '03',
+                        'titre' => 'Lorem 3',
+                        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                                     Obcaecati provident quam suscipit tempore velit. <br>
+                                                     Assumenda consequuntur expedita magnam officia recusandae reiciendis repudiandae,
+                                                      sed sit!<br>
+                                                     Accusantium dignissimos nobis rerum sequi. Molestias.'
+                        ],
+                        [
+
+                        'id' => '04',
+                        'titre' => 'Lorem 4',
+                        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                                     Obcaecati provident quam suscipit tempore velit. <br>
+                                                     Assumenda consequuntur expedita magnam officia recusandae reiciendis repudiandae,
+                                                      sed sit!<br>
+                                                     Accusantium dignissimos nobis rerum sequi. Molestias.'
+                        ],
+                        [
+
+                        'id' => '05',
+                        'titre' => 'Lorem 5',
+                        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                                     Obcaecati provident quam suscipit tempore velit. <br>
+                                                     Assumenda consequuntur expedita magnam officia recusandae reiciendis repudiandae,
+                                                      sed sit!<br>
+                                                     Accusantium dignissimos nobis rerum sequi. Molestias.'
+                        ],
+                        [
+
+                        'id' => '06',
+                        'titre' => 'Lorem 6',
+                        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                                     Obcaecati provident quam suscipit tempore velit. <br>
+                                                     Assumenda consequuntur expedita magnam officia recusandae reiciendis repudiandae,
+                                                      sed sit!<br>
+                                                     Accusantium dignissimos nobis rerum sequi. Molestias.'
+                        ]
+                ];
+
+
         /**
          * @Route("/", name="home")
          */
         public function HomeAction()
         {
 
+
             return $this->render(
                 "@App/Default/home.html.twig",
-                [
-                    'posts' => [
-                        [   'id'=>1,
-                            'titre' => 'Bla1',
-                            'contenu' =>'Lorem1 ipsum dolor sit amet, consectetur adipisicing elit. A animi consequatur cum eos est exercitationem expedita hic, libero obcaecati officia possimus quas quis, saepe, tenetur vel? Fugit molestias quis suscipit.',
-                            'resume' => 'Lorem1 ipsum dolor sit amet,'
-                        ],
-                        [   'id'=>2,
-                            'titre' => 'Bla2',
-                            'contenu' =>'Lorem2 ipsum dolor sit amet, consectetur adipisicing elit. A animi consequatur cum eos est exercitationem expedita hic, libero obcaecati officia possimus quas quis, saepe, tenetur vel? Fugit molestias quis suscipit.',
-                            'resume' => 'Lorem2 ipsum dolor sit amet,'
-                        ],
-                        [   'id'=>3,
-                            'titre' => 'Bla3',
-                            'contenu' =>'Lorem3 ipsum dolor sit amet, consectetur adipisicing elit. A animi consequatur cum eos est exercitationem expedita hic, libero obcaecati officia possimus quas quis, saepe, tenetur vel? Fugit molestias quis suscipit.',
-                            'resume' => 'Lorem3 ipsum dolor sit amet,'
-                        ]
-                    ]
+            [
+                'articles'=> $this->articles
+            ]
 
-                ]
             );
         }
+
+
+        /**
+         * @Route("/articles", name="articles")
+         */
+        public function ArticlesAction()
+        {
+
+            return $this->render(
+                "@App/Default/articles.html.twig",
+                [
+                    'articles'=> $this->articles
+                ]
+
+
+                );
+
+        }
+
+        /**
+         * @Route("/contact", name="contact")
+         */
+        public function CAction()
+        {
+            return $this->render(
+                "@App/Default/contact.html.twig"
+            );
+
+        }
+
+
     }
