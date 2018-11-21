e2c1ed85df586dcbea5c34e81733b14e29071e43
kojeje <kojeje.dev@gmail.com>
2018-11-21 23:09:26 +0100

    Avec page article unique

--- src/AppBundle/Controller/PagesController.php
+++ src/AppBundle/Controller/PagesController.php
@@ -6,90 +6,94 @@
      * Time: 14:16
      */
 
-
+//  namespace:  appel de la classe Controller
     namespace AppBundle\Controller;
-    //autoloader
+//  autoloader
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
     use Symfony\Component\Routing\Annotation\Route;
 
-
+//  création de la classe héritant de Controller
     class PagesController extends Controller
+
     {
+//      Déclarer parametre de la classe
+
+    //  parametre $articles (array in array
         private $articles =
                 [
-                    'posts' =>
+
                         [
 
-                        'id' => '01',
+                        'id' => '1',
                         'titre' => 'Lorem 1',
                         'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                                 Obcaecati provident quam suscipit tempore velit. <br>
-                                                 Assumenda consequuntur expedita magnam officia recusandae
+                                      Assumenda consequuntur expedita magnam officia recusandae
                                                   reiciendis repudiandae,
                                                   sed sit!<br>
                                                  Accusantium dignissimos nobis rerum sequi. Molestias.'
                         ],
                         [
 
-                        'id' => '02',
+                        'id' => '2',
                         'titre' => 'Lorem 2',
                         'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                                     Obcaecati provident quam suscipit tempore velit. <br>
-                                                     Assumenda consequuntur expedita magnam officia recusandae
-                                                      reiciendis repudiandae,
-                                                      sed sit!<br>
-                                                     Accusantium dignissimos nobis rerum sequi. Molestias.'
+                                      Assumenda consequuntur expedita magnam officia recusandae
+                                                  reiciendis repudiandae,
+                                                  sed sit!<br>
+                                                 Accusantium dignissimos nobis rerum sequi. Molestias.'
                         ],
                         [
 
-                        'id' => '03',
+                        'id' => '3',
                         'titre' => 'Lorem 3',
                         'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                                     Obcaecati provident quam suscipit tempore velit. <br>
-                                                     Assumenda consequuntur expedita magnam officia recusandae reiciendis repudiandae,
-                                                      sed sit!<br>
-                                                     Accusantium dignissimos nobis rerum sequi. Molestias.'
+                                      Assumenda consequuntur expedita magnam officia recusandae
+                                                  reiciendis repudiandae,
+                                                  sed sit!<br>
+                                                 Accusantium dignissimos nobis rerum sequi. Molestias.'
+
                         ],
                         [
 
-                        'id' => '04',
+                        'id' => '4',
                         'titre' => 'Lorem 4',
                         'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                                     Obcaecati provident quam suscipit tempore velit. <br>
-                                                     Assumenda consequuntur expedita magnam officia recusandae reiciendis repudiandae,
-                                                      sed sit!<br>
-                                                     Accusantium dignissimos nobis rerum sequi. Molestias.'
+                                      Assumenda consequuntur expedita magnam officia recusandae
+                                                  reiciendis repudiandae,
+                                                  sed sit!<br>
+                                                 Accusantium dignissimos nobis rerum sequi. Molestias.'
                         ],
                         [
 
-                        'id' => '05',
+                        'id' => '5',
                         'titre' => 'Lorem 5',
                         'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                                     Obcaecati provident quam suscipit tempore velit. <br>
-                                                     Assumenda consequuntur expedita magnam officia recusandae reiciendis repudiandae,
-                                                      sed sit!<br>
-                                                     Accusantium dignissimos nobis rerum sequi. Molestias.'
+                                      Assumenda consequuntur expedita magnam officia recusandae
+                                                  reiciendis repudiandae,
+                                                  sed sit!<br>
+                                                 Accusantium dignissimos nobis rerum sequi. Molestias.'
                         ],
                         [
 
-                        'id' => '06',
+                        'id' => '6',
                         'titre' => 'Lorem 6',
                         'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                                     Obcaecati provident quam suscipit tempore velit. <br>
-                                                     Assumenda consequuntur expedita magnam officia recusandae reiciendis repudiandae,
-                                                      sed sit!<br>
-                                                     Accusantium dignissimos nobis rerum sequi. Molestias.'
-                        ]
+                                      Assumenda consequuntur expedita magnam officia recusandae
+                                                  reiciendis repudiandae,
+                                                  sed sit!<br>
+                                                 Accusantium dignissimos nobis rerum sequi. Molestias.'
+                        ],
                 ];
 
-
+//      Routeur page accueil
         /**
          * @Route("/", name="home")
          */
+//      Méthode pour afficher la vue accueil
         public function HomeAction()
         {
 
-
+//          retourne le contenu du fichier 'home.html.twig' un array avec l'index "articles" qui contient lui-même la variable $article
             return $this->render(
                 "@App/Default/home.html.twig",
             [
@@ -99,7 +103,7 @@
             );
         }
 
-
+//      Méthode pour afficher la vue articles
         /**
          * @Route("/articles", name="articles")
          */
@@ -116,17 +120,31 @@
                 );
 
         }
-
+//      Méthode pour afficher la vue contact
         /**
          * @Route("/contact", name="contact")
          */
-        public function CAction()
+        public function ContactAction()
         {
             return $this->render(
                 "@App/Default/contact.html.twig"
             );
+//      Méthode pour afficher la vue de chaque article
+        }
+        /**
+         * @Route("/article/{id}", name="article")
+         */
+
+        public function ArticleAction($id)
+        {
+            $id = $id-1;
+            return $this->render(
+                "@App/Default/article.html.twig",
+                [
+                    'article'=> $this->articles[$id]
+                ]
+            );
 
         }
-
 
     }
